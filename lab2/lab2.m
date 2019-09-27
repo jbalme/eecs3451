b3ebf8cfb41fd24bb010b423f79bcf5e91adac36
Jonathan Baldwin <jbaldwin9182@gmail.com>
2019-09-27 09:06:39 -0400

    Finish up Q1-4

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -1,8 +1,10 @@
 %% EECS 3451 Lab1
 % *AUTHORS*
-% * Jonathan Baldwin 212095691
-% * Mark Savin 212921128
-% * Sarwat Shaheen 214677322
+% 
+% 
+% * Jonathan Baldwin
+% * Mark Savin
+% * Sarwat Shaheen
 
 %% Problem 1
 % As the frequency varies, so does the pitch proportionally. Doubling the
@@ -41,7 +43,10 @@ end
 % the lower the smalping frequency the more muffled the noise becomes
 
 
-%% Problem 3
+%% Problem 3 A
+% Unlike in Problem 2, changing the sampling rate doesn't seem to affect
+% the quality of the sound.
+
 
 [y,Fs] = audioread('P_3_1.wav');
 plot([1:size(y)]./Fs,y);
@@ -55,6 +60,10 @@ for Fs2=[2000,4000,6000,12000]
    playsound(y2,Fs2);
 end
 
+%% Problem 3 b)
+% This time, much like in Problem 2, the sampling rate affects the quality
+% of the sound greatly.
+
 [y,Fs] = audioread('P_3_2.wav');
 plot([1:size(y)]./Fs,y);
 
@@ -67,13 +76,55 @@ for Fs2=[2000,4000,6000,12000]
    playsound(y2,Fs2);
 end
 
-%% Problem 4
+%% Problem 4a - double
+% Running the signal through double doesn't seem to affect the sound at
+% all.
 
+[y,Fs] = audioread('P_3_2.wav');
+plot([1:size(y)]./Fs,y);
 
+y2=double(y);
+fprintf('Playing at input frequency %d Hz, doubled to %d\n', Fs, Fs*2);
+playsound(y2,Fs*2);
+
+for Fs2=[2000,4000,6000,12000]
+   y2=resample(y,Fs2,Fs);
+   y3=double(y2);
+   fprintf('Playing resampled to %d Hz, then doubled to %d\n', Fs2, Fs2*2);
+   playsound(y3,Fs2*2);
+end
+
+%% Problem 4b - fliplr
+% Running the signal through fliplr flips the left and right channels. As
+% our voice sample is centered, the sound output doesn't perceptibly
+% change.
+
+[y,Fs] = audioread('P_3_2.wav');
+plot([1:size(y)]./Fs,y);
+
+y2=fliplr(y);
+fprintf('Playing at input frequency %d Hz\n', Fs);
+playsound(y2,Fs);
+
+%% Problem 4b - flipud
+% Running the signal through flipud reverses the sound. It sounds like
+% something out of the Exorcist.
+
+[y,Fs] = audioread('P_3_2.wav');
+plot([1:size(y)]./Fs,y);
+
+y2=flipud(y);
+fprintf('Playing at input frequency %d Hz\n', Fs);
+playsound(y2,Fs);
 
 %% Function Definitions
 
 function playsound(y, Fs)
+    % On Linux, refuses to play at anything other than 44100 Hz
+    if isunix()
+        y = resample(y,44100,Fs);
+        Fs = 44100;
+    end
     player = audioplayer(y,Fs);
     playblocking(player);
 end
