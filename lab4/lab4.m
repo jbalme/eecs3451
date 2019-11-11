e32c645aa95e3b61df5358eb2c6244674b76f5f3
Jonathan Baldwin <jbaldwin9182@gmail.com>
2019-11-10 19:44:36 -0500

    finalize, add pdf

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,26 +1,60 @@
-%% Q1 
+%% EECS 3451 Lab 4
 
-Fs = 10e3;              % 10 kHz sampling rate
+
+%% AUTHORS
+% 
+% * Jonathan Baldwin (212095691)
+% * Mark Savin       (212921128)
+% * Sarwat Shaheen   (214677322)
+%
+
+%% Q1 - Fourier Coefficients of a Rectangular Pulse
+
+Fs = 100e3;             % 100 kHz sampling rate
 Ts = 1/Fs;              % sampling period
 
-t=0:Ts:1-Ts;
+T=1e-3;
 
-d=0:1e-3:1;             % 1ms period
-w=0.1e-3;               % .1 ms pulse width
+t=-T/2:Ts:T/2;
 
-X = pulstran(t,d,@rectpuls,w);
+d=-1:T:1;                % 1ms period
+w=0.1e-3;                % .1 ms pulse width
 
-figure
-plot(t,X);
-xlim([0 .01]);
-ylim([0 1]);
+x = rectpuls(t,w);
 
-figure
-stem(freqdomain(length(X),Fs),normfft(X));
+N = length(x);
+k=-(N-1)/2:(N-1)/2;
+C=zeros(1,length(k));
+n=t/Ts;
 
-%% Q2
-% To get the result of the fft to be 501 equi-spaced frequencies between 0
-% and $\pi$, we need 501 samples of [1 2 3 4 5] at $\pi$ sample rate.
+for i1=1:length(k)
+    for i2=1:length(x)
+        C(i1)=C(i1)+1/N*x(i2)*exp(-1i*2*pi*k(i1)*n(i2)/N);
+    end
+end
+
+figure;
+sgtitle('Fourier Coefficients of a 0.1ms Rectangular Pulse');
+
+plot1 = subplot(3,1,1);
+plot(t,x);
+title('0.1ms Rectangular Pulse');
+xlabel('Time');
+ylabel('Value');
+
+plot2 = subplot(3,1,2);
+stem(k, abs(C));
+title('Magnitude of the `k`-th Fourier Coefficient');
+xlabel('k');
+ylabel('Magnitude');
+
+plot3 = subplot(3,1,3);
+stem(k, angle(C)*130/pi);
+title('Phase of the `k`-th Fourier Coefficient');
+xlabel('k');
+ylabel('Phase');
+
+%% Q2 - DTFT of x(n)={1,2,3,4,5} from 0 to \pi
 
 n = -1:3; 
 x = 1:5;                  
@@ -31,126 +65,223 @@ W = exp(-1i * pi * n' * w);
 
 X = x * W ;
 
-mag = abs(X); 
+figure;
+sgtitle('Discrete Time Fourier Transform of x(t)={1,2,3,4,5}');
+
 subplot(2, 2, 1);
-plot(k/500,mag);
+plot(w,abs(X));
 title('Magnitude Plot'); 
 xlabel('Frequency'); 
 ylabel('Magnitude');
 
-angC = angle(X);
 subplot(2, 2, 2);
-plot(k/500,angC);
+plot(w,angle(X));
 title('Angle Plot'); 
 xlabel('Frequency'); 
 ylabel('Angle');
 
-real = real(X);
 subplot(2, 2, 3);
-plot(k/500,real);
+plot(w,real(X));
 title('Real Part'); 
 xlabel('Frequency'); 
 ylabel('Real');
 
-img = imag(X);
 subplot(2, 2, 4);
-plot(k/500,img);
+plot(w,imag(X));
 title('Imaginary Part'); 
 xlabel('Frequency'); 
 ylabel('Imaginary');
 
+%% Q3 Gaussian Pulse
+% When we take the zero-centered FFT of a gaussian pulse, the magnitude of
+% the FFT is shaped like a gaussian pulse.
 
-%% Q3 Gaus Puls
-t=0:1e-2:5;
-d=2.5;
+Fs = 50;
+Ts = 1/Fs;
+sigma = 0.05;
 
-in = pulstran(t,d,@gauspuls,1,10);
-out = fft(in,length(in));
+t=-1:Ts:1;
 
-figure
+x = 1/sqrt(2*pi*sigma^2)*exp(-t.^2/(2*sigma^2));
 
-plot(t,in);
-hold on
-plot(t,out);
-plot(t,imag(out));
-hold off
+N = length(x);
+NFFT = N;
+
+F = Fs*(-NFFT/2:NFFT/2-1)/NFFT;
+Y = fftshift(fft(x, NFFT));     % Use fftshift to zero-center the fft
+
+figure;
+sgtitle("Gaussian Pulse: Time Domain vs Frequency Domain");
+
+plot1 = subplot(3,1,1);
+plot(t,x);
+title("Gaussian Pulse (\sigma=" + num2str(sigma) + ")");
+xlabel("Time");
+ylabel("Value");
+
+plot2 = subplot(3,1,2);
+plot(F, abs(Y)/N, 'r');
+title("FFT of Gaussian Pulse")
+xlabel("Frequency");
+ylabel("Magnitude");
 
 %% Problem - 4
-[audio_in, audio_freq_sampl] = audioread('audio.wav');
+% Here we do frequency analysis on the Blues Brothers clip from Lab 2.
+% When we look at the magnitude response of the audio signal, we can see
+% the largest peaks in the 400-600 Hz range.
 
-audio_fft = fft(audio_in);
+[x, Fs] = audioread('audio.wav');
 
-%Magnitude and phase plots of the audio signal used from back in Lab 2
+N = length(x);
+NFFT = 2^12;
 
-figure
-plot(abs(audio_fft));
-title('Magnitude plot of Input Audio in Frequency Spectrum');
-xlabel('Frequency(Hz)');
-ylabel('Magnitude component');
+X = fft(x, NFFT);
+F = Fs*(0:NFFT-1)/NFFT;
 
-figure
-plot(angle(audio_fft));
-title('Phase plot of Input Audio in Frequency Spectrum');
-xlabel('Frequency(Hz)');
-ylabel('Phase component');
+figure;
+sgtitle('Frequency Analysis of "Blues Brothers" Audio Sample');
+
+plot1 = subplot(2,1,1);
+plot(F(1:NFFT/2),abs(X(1:NFFT/2)));
+title('Magnitude response of the audio signal');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude response');
+
+plot2 = subplot(2,1,2);
+plot(F(1:NFFT/2),angle(X(1:NFFT/2)));
+title('Phase response of the audio signal');
+xlabel('Frequency (Hz)');
+ylabel('Phase response (radians)');
 
 
 %% Problem - 5
-Fs = 10000;
-t = 0:1/Fs:1;
+% To increase the frequency resolution, we can either increase the number
+% of samples while keeping sample rate constant (which has the downside of 
+% needing to capture and process more samples), or decrease the sampling
+% rate while keeping number of samples constant (which has the downside of 
+% lowering the maximum frequency we can resolve.) The former is preferable
+% here.
+
+Ts = 0.999e-3;
+Fs = 1/Ts;
+t = 0:Ts:1;
 f1 = 1000;
 x = sin(2*f1*t) + (1/3)*sin(2*3*f1*t) + (1/5)*sin(2*5*f1*t);
+
+N = length(X);
+NFFT = N;
+F = Fs*(0:NFFT-1)/NFFT;
+X = fft(x, NFFT);
+
+figure;
+sgtitle("FFT of a deterministic periodic signal");
+
+subplot(3,2,1);
+plot(t, x);
 xlabel("Time");
 ylabel("Signal");
-title("Plot of signal in the time domain");
-plot(t, x);
+title("x(t) in time domain");
 
-stem(freqdomain(length(x),Fs),normfft(x));
-xlabel("Frequency")
-ylabel("Signal")
-title("Plot of signal in the frequency domain")
+subplot(3,2,3);
+stem(F,abs(X));
+xlabel("Frequency");
+ylabel("Magnitude");
+title("|X(t)| in frequency domain");
+
+subplot(3,2,5);
+stem(F,angle(X));
+xlabel("Frequency");
+ylabel("Phase");
+title("\theta(X(t)) in frequency domain");
+
+Ts = 0.999e-3;
+Fs = 1/Ts;
+t = 0:Ts:2;
+f1 = 1000;
+x = sin(2*f1*t) + (1/3)*sin(2*3*f1*t) + (1/5)*sin(2*5*f1*t);
+
+N = length(X);
+NFFT = N;
+F = Fs*(0:NFFT-1)/NFFT;
+X = fft(x, NFFT);
+
+subplot(3,2,2);
+plot(t, x);
+xlabel("Time");
+ylabel("Signal");
+title("x(t) (2x samples)");
+
+subplot(3,2,4);
+stem(F,abs(X));
+xlabel("Frequency");
+ylabel("Magnitude");
+title("|X(t)| (2x samples)");
+
+subplot(3,2,6);
+stem(F,angle(X));
+xlabel("Frequency");
+ylabel("Phase");
+title("\theta(X(t)) (2x samples)");
 
 %% Problem - 6
-Fs = 10000;
-t = 0:1/Fs:1;
+% When we plot the noisy signal against the original, we can see that the
+% noise is a lot more noticeable in the time domain, compared to the
+% frequency domain.
+
+Ts = 0.999e-3;
+Fs = 1/Ts;
+t = 0:Ts:1;
 f1 = 1000;
 x = sin(2*f1*t) + (1/3)*sin(2*3*f1*t) + (1/5)*sin(2*5*f1*t);
+xNoisy = x + randn(size(x));
+
+N = length(X);
+NFFT = N;
+F = Fs*(0:NFFT-1)/NFFT;
+X = fft(x, NFFT);
+XNoisy = fft(xNoisy, NFFT);
+
+figure;
+sgtitle("Time vs Frequency of Original vs Noisy Signal");
+
+subplot(3,2,1);
+plot(t,x);
 xlabel("Time");
 ylabel("Signal");
-title("Plot of signal in the time domain");
-plot(t, x);
-stem(freqdomain(length(x),Fs),normfft(x));
+title("Original, time domain");
+
+subplot(3,2,3);
+stem(F,abs(X));
 xlabel("Frequency")
 ylabel("Signal")    
-title("Plot of signal in the frequency domain")
+title("Original, magnitude response");
 
+subplot(3,2,5);
+stem(F,(angle(X)));
+xlabel("Frequency")
+ylabel("Signal")    
+title("Original, phase response");
+
+subplot(3,2,2);
+plot(t,xNoisy);
 xlabel("Time");
 ylabel("Signal");
-title("Plot of noisy signal in the time domain");
-noisySignal = x + (randn(size(x)) + 5);
-plot(t, noisySignal);
+title("Noisy, time");
 
+subplot(3,2,4);
+stem(F,abs(XNoisy));
 xlabel("Frequency")
 ylabel("Signal")    
-title("Plot of noisy signal in the frequency domain")
-stem(freqdomain(length(noisySignal),Fs),normfft(noisySignal))
+title("Noisy, magnitude response");
 
-%% fns
+subplot(3,2,6);
+stem(F,(angle(XNoisy)));
+xlabel("Frequency")
+ylabel("Signal")    
+title("Noisy, phase response");
 
-function y = freqdomain(N, Fs)
-    if mod(N,2)==0
-        k=-N/2:N/2-1;
-    else
-        k=-(N/2):(N-1)/2;
-    end
-    T=N/Fs;
-    y=k/T;
-end
-
-function y = normfft(x, varargin)
-    if length(varargin) == 0
-        y = fftshift(fft(x)/length(x));
-    else
-        y = fftshift(fft(x,varargin{0})/length(x));
-    end
-end
+%% What we learned
+% In this lab we learned how to calculate Fourier coefficients in MATLAB,
+% how to use the Fast-Fourier Transform to convert from the time domain to
+% the frequency domain, how to increase the frequency resolution of the
+% FFT, and how noise is less perceptible in the frequency domain.
